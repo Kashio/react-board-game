021bbabbf24d76acadba53e472c152d148e12be5
Roy Mor <roy.mor173@gmail.com>
2019-03-22 00:55:33 +0200

    add webpack with basic react app component for layout

---
+++ client/src/layout/app/app.jsx
@@ -0,0 +1,20 @@
+import React from 'react';
+import './app.scss';
+
+class App extends React.Component {
+    constructor() {
+        super();
+    }
+
+    render() {
+        return (
+            <div className="app">
+                <main>
+                    App
+                </main>
+            </div>
+        );
+    }
+}
+
+export default App;
