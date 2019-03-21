f1c530a8b3a7b0f9522452739cfbbdda1afd1067
Roy Mor <roy.mor173@gmail.com>
2019-03-22 00:57:39 +0200

    add board component used to build game board with boxes for any NxM board

---
+++ client/src/components/board/board.jsx
@@ -0,0 +1,25 @@
+import React from 'react';
+import './board.scss';
+import Box from '@/box/box';
+import classNames from 'classnames';
+
+const Board = ({board, width, height, boxTemplate, onBoxClick}) => {
+    return <div className="board" style={{width, height}}>
+        {board.map((row, i) => <div key={i} className="row" style={{height: height / board.length}}>
+                {row.map((column, j) => {
+                    const boxClass = classNames({
+                        top: i !== 0,
+                        right: j !== row.length - 1,
+                        bottom: i !== board.length - 1,
+                        left: j !== 0
+                    });
+                    return <Box key={i + j} className={boxClass} width={width / row.length} height={height / board.length} row={i} column={j} onClick={onBoxClick}>
+                        {boxTemplate(i, j)}
+                    </Box>;
+                })}
+            </div>
+        )}
+    </div>;
+};
+
+export default Board;
